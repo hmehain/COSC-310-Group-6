9b3fe949b5876ebee1bbe08cfb3a06c24b414820
Heather Mehain <heather.mehain@alumni.ubc.ca>
2019-02-06 19:48:35 -0800

    I just moved Meredith's Patient class inside the src folder.

--- Patient.java
+++
@@ -1,8 +0,0 @@
-
-public class Patient {
-
-	//I'm not sure if any of these need to be private or not
-	public int age;
-	public String name;
-	
-}
---
+++ src/ConvoBot/Patient.java
@@ -0,0 +1,8 @@
+package ConvoBot;
+
+public class Patient {
+	
+	//I'm not sure if any of these need to be private or not
+		public int age;
+		public String name;
+}
