cfa282245d21fa48362c078a1ba6051774aedebf
Heather Mehain <heather.mehain@alumni.ubc.ca>
2019-02-15 11:52:53 -0800

    I created new classes for Advice, Discussion, SmallTalk, and Goodbye because I didn't want Joel or Meredith to have any trouble with pushing what they are working on. When they are done their work, I will merge the classes.

--- src/ConvoBot/Conversation.java
+++ src/ConvoBot/Conversation.java
@@ -2,30 +2,37 @@ package ConvoBot;
 
 import Topics.*;
 
+import Topics.*;
+
 public class Conversation {
 
 	Patient patient;
-	static Topic[] conversationTopic = {new Greetings(), new SmallTalk(), new Discussion(), new Advice(), new Goodbye()};
-	boolean discussionOn;
-	
+	public static boolean startNextTopic = false;
+
 	public Conversation() {
 		Patient p = new Patient();
-		startConversation();
-		endConversation();
-		
-	}
-	
-	public void startConversation() {
-		
-		for(Topic t: conversationTopic) {
-			t.startTopic();
-			
-			t.endTopic();
+		Topic.currentTopic = 0;
+		while (Topic.currentTopic < 5) {
+			//System.out.println("////current Topic: " + Topic.currentTopic);
+			switch (Topic.currentTopic) {
+			case 0:
+				Greetings.startTopic();
+				break;
+			case 1:
+				SmallTalk2.startTopic();
+				break;
+			case 2:
+				Discussion2.startTopic();
+				break;
+			case 3:
+				Advice2.startTopic();
+				break;
+			case 4:
+				Goodbye2.startTopic();
+				break;
+			}
+
 		}
-		
-	}
-	
-	public void endConversation() {
-		discussionOn = false;
+		System.out.println("*********Ending Conversation*******");
 	}
 }
---
+++ src/Topics/Goodbye2.java
@@ -0,0 +1,17 @@
+package Topics;
+
+public class Goodbye2 extends Topic {
+
+	String[] messages = {"Goodbye 0"};
+
+
+	public static void startTopic() {
+		System.out.println("*******Starting Goodbye******");
+		
+		currentTopic++;
+		
+	}
+	
+
+
+}
