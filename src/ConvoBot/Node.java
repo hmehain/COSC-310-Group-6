37394189ae2722e20624aec0f825b390a511c444
haodanxue <35201266+haodanxue@users.noreply.github.com>
2019-02-16 01:54:08 -0800

    generate getters and setters

--- src/ConvoBot/Node.java
+++ src/ConvoBot/Node.java
@@ -4,12 +4,30 @@ import java.util.PriorityQueue;
 
 public class Node implements Comparable<Node> {
 	
-	// currently no getter/setter methods (except for weight), implement later?
-	Subject subject;
-	PriorityQueue<Edge> edgesToNode;
-	PriorityQueue<Edge> edgesFromNode;
+	private Subject subject;
+	private PriorityQueue<Edge> edgesToNode;
+	private PriorityQueue<Edge> edgesFromNode;
 	
 	private Double weight;
+	
+	//Enable or Disable nodes
+	private boolean enabled; //Set it to either true (enabled) or false (disabled)
+	
+	//getter and setter for enabled
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		// if enabled set to true, update edges away from node
+	
+		 //if (!this.getSubjectName().equals("centerNode")) { 
+				this.enabled = enabled;
+			 //for (Edge e : edgesFromNode) {
+				 //e.updateEdgeWeight();
+		//}
+		//}
+	}
 	
 	// creates a new node with no to/from edges
 	public Node(Subject subject) {
@@ -18,26 +36,59 @@ public class Node implements Comparable<Node> {
 		
 		edgesToNode = new PriorityQueue<>();
 		edgesFromNode = new PriorityQueue<>();
+		this.setEnabled(true);
 	}
 	
-	
+	//getter and setter methods for weight, subject, edgesToNode, and edgesFromNode
 	public Double getWeight() {
 		return this.weight;
 	}
+	
 	public void setWeight(Double weight) {
 		this.weight = weight;
 	}
 	
+	public Subject getSubject() {
+		return this.subject;
+	}
+	
+	public void setSubject(Subject subject) {
+		this.subject = subject;
+	}
+	
+	public PriorityQueue<Edge> getEdgesToNode() {
+		return edgesToNode;
+	}
+
+
+	public void setEdgesToNode(PriorityQueue<Edge> edgesToNode) {
+		this.edgesToNode = edgesToNode;
+	}
+
+
+	public PriorityQueue<Edge> getEdgesFromNode() {
+		return edgesFromNode;
+	}
+
+
+	public void setEdgesFromNode(PriorityQueue<Edge> edgesFromNode) {
+		this.edgesFromNode = edgesFromNode;
+	}
+
 	// increments weight by some amount, should only be called if node subject is a characteristic
 	public void incrementWeight() {
 		this.incrementWeight(1.0);
 	}
+	
 	public void incrementWeight(Double weight) {
 		this.setWeight(this.weight + weight);
 		
 		if (!this.getSubjectName().equals("centerNode")) {
 			for (Edge e : edgesFromNode) {
-				e.updateEdgeWeight();
+				// if edge == enabled only
+				//if (e.isEnabled() == true) {
+					e.updateEdgeWeight();
+				//}
 			}
 		}
 	}
@@ -46,13 +97,19 @@ public class Node implements Comparable<Node> {
 	public void collectWeights() {
 		Double weightSum = 0.0;
 		for (Edge e : edgesToNode) {
-			weightSum += e.getWeightOut();
+			// if edge == enabled only
+			//if (e.isEnabled() == true) {
+				weightSum += e.getWeightOut();
+			//}
 		}
 		this.setWeight(weightSum);
 		
 		if (!this.getSubjectName().equals("centerNode")) {
 			for (Edge e : edgesFromNode) {
-				e.updateEdgeWeight();
+				// if edge is enabled only
+				//if (e.isEnabled() == true) {
+					e.updateEdgeWeight();
+				//}
 			}
 		}
 	}
