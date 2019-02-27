f4e9f6536414653e139f860c2014aa0649b8d252
joelp <joelp@DESKTOP-5IG2DJS.telus>
2019-02-26 17:52:40 -0800

    More discussion improvements

--- src/Topics/Discussion.java
+++ src/Topics/Discussion.java
@@ -32,7 +32,7 @@ public class Discussion extends Topic {
 	public static String[] myMessages = { "Let's discuss further why your 0", "Earlier you said your 0",
 			"Does that have anything to do with the fact that your 0" };
 	public static String[] noMessages = { "Please go on.", "That's very interesting", "I see.",
-			"How does that make you feel?", "Could you please elaborate?" };
+			"How does that make you feel?", "Could you please elaborate?", "Please continue.", "I understand." };
 	ContextGraph contextGraph;
 
 	public Discussion(ContextGraph contextGraph) {
@@ -58,6 +58,7 @@ public class Discussion extends Topic {
 	 */
 	public String discussionRules(String input) {
 		extractKeywords(input);
+		Scanner scanner1 = new Scanner(System.in);
 		input = input.toLowerCase();
 		String output = null;
 
@@ -89,9 +90,11 @@ public class Discussion extends Topic {
 		negativeList.add("loath");
 		negativeList.add("despise");
 		negativeList.add("dislike");
+		negativeList.add("stressed");
+		negativeList.add("tired");
 
 		// Case 1: User responds with I
-		Pattern p1 = Pattern.compile("(.*)(i[^'m])(.*)");
+		Pattern p1 = Pattern.compile("(.*)(i\\s)(.*)");
 		Matcher m1 = p1.matcher(input);
 
 		// Case 2: User responds with I'm
@@ -113,21 +116,28 @@ public class Discussion extends Topic {
 		// Case 6: User responds with no
 		Pattern p6 = Pattern.compile("(.*)(no)(.*)");
 		Matcher m6 = p6.matcher(input);
-
-		if (m1.find() && !m2.find()) {
+		
+		// Case 6: user response contains "Tired"
+		Pattern p7 = Pattern.compile("(.*)(tired)(.*)");
+		Matcher m7 = p7.matcher(input);
+		
+		if (m1.matches() && !m2.matches()) {
 			System.out.println("Case 1");
 			// Case 1.1: I am
 			Pattern p1_1 = Pattern.compile("(.*)(am)(.*)");
 			Matcher m1_1 = p1_1.matcher(input);
+
+			// Case 1.2: I feel
+			Pattern p1_2 = Pattern.compile("(.*)(feel)(.*)");
+			Matcher m1_2 = p1_2.matcher(input);
+			
+			// Case 1.3: I ___ Busy ___
 
 			if (m1_1.find()) {
 				System.out.println("Case 1.1");
 				// Case 1.1.1: I am feeling BLANK
 				Pattern p1_1_1 = Pattern.compile("(.*)(feeling)(.*)");
 				Matcher m1_1_1 = p1_1_1.matcher(input);
-				
-				Pattern p1_1_2 = Pattern.compile("(.*)(feel)(.*)");
-				Matcher m1_1_2 = p1_1_2.matcher(input);
 
 				if (m1_1_1.find()) {
 					System.out.println("case 1.1.1");
@@ -142,16 +152,13 @@ public class Discussion extends Topic {
 						output = "I'm sorry to hear that, what's wrong?";
 					}
 
-					// Case 1.1.2 I am BLANK. EX I am worried/sad
-				} else if (m1_1_2.matches()) { // I feel
-					System.out.println("Case 1.1.2");
 				} else {
-					System.out.println("case 1.1.3");
+					System.out.println("case 1.1.2");
 					String keyword = m1_1.group(3);
 					keyword = keyword.replaceAll("\\s+", "");
 					System.out.println("Keyword:" + keyword);
 					if (happyList.contains(keyword.toLowerCase())) {
-						output = "I'm glad to hear that you feel good! Is there anything you would like to talk about?";
+						output = "I'm glad to hear that you feel good! Keep it up by getting into healthy habits. Do you excercise?";
 						// Change graph weight
 					} else if (sadList.contains(keyword.toLowerCase())) {
 						// Change graph weights
@@ -160,6 +167,21 @@ public class Discussion extends Topic {
 						output = noMessages[(int) (Math.random() * noMessages.length)];
 					}
 				}
+			}else if(m1_2.find()) { // I feel
+				System.out.println("Case 1.2");
+				String keyword = m1_2.group(3);
+				keyword = keyword.replaceAll("\\s+", "");
+
+				if (happyList.contains(keyword.toLowerCase())) {
+					output = "I'm glad to hear that you're feeling good! Keep it up by getting into healthy habits. Do you excercise?";
+					// Change graph weight
+				} else if (sadList.contains(keyword.toLowerCase()) ||negativeList.contains(keyword.toLowerCase())) {
+					// Change graph weights
+					output = "I'm sorry to hear that, what's wrong?";
+				}else {
+					output = noMessages[(int) (Math.random() * noMessages.length)];
+				}
+				
 			} else { // Case 1.3: I BLANK. EX I hate BLANK, I love BLANK, I procrastinate, ECT
 				System.out.println("Case 1.3");
 				String[] words = m1.group(3).split(" "); // Splits the String into individual words
@@ -202,7 +224,7 @@ public class Discussion extends Topic {
 				keyword = m2_1.group(3);
 				keyword = keyword.replaceAll("\\s+", "");
 				if (happyList.contains(keyword.toLowerCase())) {
-					output = "I'm glad to hear that you're feeling good!";
+					output = "I'm glad to hear that you're feeling good! What would you like to talk about today?";
 					// Change graph weight
 				} else if (sadList.contains(keyword.toLowerCase())) {
 					// Change graph weights
@@ -222,17 +244,16 @@ public class Discussion extends Topic {
 				keyword = keyword.replaceAll("\\s+", "");
 
 				if (happyList.contains(keyword.toLowerCase())) {
-					Scanner scanner1 = new Scanner(System.in);
 					System.out.println("I'm glad to hear that you're " + keyword
 							+ "! Keep it up by getting into healthy habits. Do you excercise?");
 					String temp = scanner1.next();
 					temp = temp.toLowerCase();
 					Pattern p2_3_1 = Pattern.compile("(.*)(yes)(.*)");
 					Matcher m2_3_1 = p2_3_1.matcher(temp);
-					Pattern p2_3_2 = Pattern.compile("(.*+)(do)(.*+)");
+					Pattern p2_3_2 = Pattern.compile("(.*)(do)(.*)");
 					Matcher m2_3_2 = p2_3_2.matcher(temp);
-					
-					if (m2_3_1.matches() || m2_3_2.matches()) {
+
+					if (m2_3_1.find() || m2_3_2.find()) {
 						output = "Good for you! Excercise has been proven to improve concentration, helps you sleep better,"
 								+ " and reduce anxiety and depression. What else you would like to talk about?";
 					} else {
@@ -248,23 +269,61 @@ public class Discussion extends Topic {
 				}
 
 			}
-		} else if (m3.matches()) { // Case 3, user response contains stressed
+		} else if (m3.find()) { // Case 3, user response contains stressed
 			System.out.println("Case 3");
-			output = "I'm sorry to hear that, make sure to block out time for yourself to unwind. \nRemeber, things you can't change aren't worth worrying about!";
-		} else if (m4.matches()) { // Case 4, user response contains depressed
+			output = "I'm sorry to hear that, make sure to block out time for yourself to unwind. \nRemeber, things you can't change aren't worth worrying about! Whats been stressing you out?";
+		} else if (m4.find()) { // Case 4, user response contains depressed
 			System.out.println("Case 4");
 			output = sampleMessages[6];
-		} else if (m5.matches()) { // case 5, user response contains yes
+		} else if (m5.find()) { // case 5, user response contains yes
 			System.out.println("Case 5");
 			output = "That's good! I'm glad you're being proactive about you're problems. What else would you like to talk about?";
-		} else if (m6.matches()) { // Case 6, user response contains no
+		} else if (m6.find()) { // Case 6, user response contains no
 			System.out.println("Case 6");
 			output = "I'd reccomend trying it, but if you aren't interested is there anything else you think would help?";
+		} else if (m7.find()){
+			System.out.println("Case 7");
+			System.out.println("Do you excercise? It can help with sleeping better.");
+			String temp = scanner1.next().toLowerCase();
+			Pattern p7_1 = Pattern.compile("(.*)(yes)(.*)");
+			Matcher m7_1 = p7_1.matcher(temp);
+			Pattern p7_2 = Pattern.compile("(.*)(do)(.*)");
+			Matcher m7_2 = p7_2.matcher(temp);
+
+			if (m7_1.find() || m7_2.find()) {
+				System.out.println("Good for you! Excercise has been proven to improve concentration, helps you sleep better,"
+						+ " and reduce anxiety and depression. Since you're still tired do you have a regular sleep schedule?");
+				String temp1 = scanner1.next().toLowerCase();
+				m7_1 = p7_1.matcher(temp1);
+
+				if(m7_1.find()) {
+					System.out.println("Thats good, keeping a regular sleep schedule has been proven to improve sleep quality. Make sure you keep it up! Are you going to bed early enough?"
+							+ "\n The national sleep foundation reccomends 7 - 9 hours for adults between 18 and 64. "
+							+ "\n Apart from that you seem to have excellent sleep habits, if you have trouble falling asleep that can be caused from stress or anxiety."
+							+ "\n Have you been feeling stressed or anxious lately?");
+					String temp2 = scanner1.next().toLowerCase();
+					m7_1 = p7_1.matcher(temp2);
+					if(m7_1.find()) {
+						output = sampleMessages[6];
+					}else {
+						output = "Thats good. Just make sure you're getting enough sleep then.";
+					}
+				}else {
+					output = "You should try sleeping at around the same time every night. It's been proven to improve the quality of sleep.";
+				}
+			} else {
+				System.out.println("You should try it. Excercise has been proven to improve concentration, helps you sleep better,"
+						+ " and reduces anxiety and depression. Do you have a reagular sleep schedule?");
+				temp = scanner1.next().toLowerCase();
+				if(m7_1.find()) {
+					output = "Thats good, keeping a regular sleep schedule has been proven to improve sleep quality. Make sure you keep it up!";
+				}
+			}
 		} else {
 			System.out.println("No case match");
 			output = noMessages[(int) (Math.random() * noMessages.length)];
 		}
-
+		
 		return output;
 
 	}
